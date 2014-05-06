f6442052b66091b4ba7020e064edcd98ad7951e3
Alexandre Gonçalves <a.goncalves@runtime-revolution.com>
2014-05-06 11:45:03 +0100

    changed default shell to zsh. Added some plugins to zsh

--- components/packages.sh
+++ components/packages.sh
@@ -1,5 +1,6 @@
 
 source ~/.bash_profile
+
 echo "Installing postgres, node and heroku-toolbelt..."
 #Postgres
 brew install postgres --no-python >> $log_file 2>&1
@@ -11,5 +12,24 @@ brew install node >> $log_file 2>&1
 #Heroku
 brew install heroku-toolbelt >> $log_file 2>&1
 heroku plugins:install git://github.com/ddollar/heroku-config.git >> $log_file 2>&1
+
+
+#ZSH
+chsh -s $(which zsh)
+if [[ -f /etc/zshenv ]]; then
+  echo "Fixing OSX zsh environment bug ..."
+    sudo mv /etc/{zshenv,zshrc}
+fi
+
+curl -L http://install.ohmyz.sh | sh
+
+sed -i -e '/ZSH_THEME=/ c\
+ZSH_THEME="bira"
+' ~/.zshrc
+
+sed -i -e '/plugins=/ c\
+plugins=(git rails ruby rvm heroku brew)
+' ~/.zshrc
+
 
 echo "Installation completed."
--- machine_setup.sh
+++ machine_setup.sh
@@ -8,6 +8,7 @@ echo '## Welcome to runtime-revoluton ##'
 echo '##################################'
 
 echo -e "\nThis script will setup your machine for ruby on rails development\n"
+echo -e "Make sure you have xcode installed from the app store."
 
 ./components/brew.sh
 
